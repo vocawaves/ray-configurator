3f386173d795c24ba69be3a8cce4b255c4e4de4d
David Ralph <me@davidcralph.co.uk>
2023-03-28 12:03:19 +0100

    improve build experience

--- build_win.bat
+++ build_win.bat
@@ -1,4 +1,3 @@
-rmdir /s /q __pycache__ build dist
-del *.spec
-
-pyinstaller --noconfirm --noconsole --onefile --clean --collect-data sv_ttk --name "ray-configurator" "./ray_configurator/gui.py"
+rmdir /s /q __pycache__ build dist rayversion.txt
+python version.py
+pyinstaller --noconfirm --noconsole --onefile --clean --version-file="./rayversion.txt" --name "ray-configurator" "./ray_configurator/gui.py"
